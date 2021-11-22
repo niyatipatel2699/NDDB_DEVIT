45759e116be95de89191a3cb4e66f998df929512
niyati.patel <niyati.patel@devitpl.com>
2021-11-22 18:40:27 +0530

    registration response changes

--- app/src/main/java/com/nddb/kudamforkurien/data/remote/responses/RegistrationRersponse/RegistrationData.kt
+++ app/src/main/java/com/nddb/kudamforkurien/data/remote/responses/RegistrationRersponse/RegistrationData.kt
@@ -6,6 +6,7 @@ data class RegistrationData (
     var last_name: String? = null,
     var phone_number: String? = null,
     var is_facilitator : Int? = 0,
+    var gender : String? = null,
     var lang_id : Int? = 0,
     var is_Registered : Int? = 0,
     var state: String? = null,
